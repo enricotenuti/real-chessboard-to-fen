a8db7758e83796ab6b0002ea1ab065897c4489b3
enricotenuti <tenutz_27@outlook.it>
2024-05-03 13:01:23 +0200

    last commit message

--- Camera.py
+++ Camera.py
@@ -2,17 +2,23 @@
 import time
 from picamera2 import Picamera2, Preview
 import libcamera
+from datetime import datetime
 
 
 def takePhoto():
     picam = Picamera2()
-
+    current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    print("Current date & time : ", current_datetime)
     config = picam.create_still_configuration({"size": (1944, 1944)})
 
     picam.configure(config)
-
+    
     picam.start()
     #time.sleep(0)
-    picam.capture_file("images/current_board.jpeg")
+    path = "images/" + current_datetime + ".jpeg"
+    picam.capture_file(path)
 
     picam.close()
+    
+    
+takePhoto()
---
+++ timestamp.py
@@ -0,0 +1,7 @@
+# import module
+from datetime import datetime
+
+# get current date and time
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+print("Current date & time : ", current_datetime)
+
